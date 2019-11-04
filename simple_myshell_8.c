b4824471448e62ba3a180210badb361dee55b7a1
Leewongi0731 <wglee0731@naver.com>
2019-11-05 00:43:15 +0900

    add command option (&:background)

--- simple_myshell_8.c
+++ simple_myshell_8.c
@@ -7,6 +7,7 @@
 const char *prompt = "myshell> ";
 char* cmdvector[MAX_CMD_ARG];
 char  cmdline[BUFSIZ];
+int background_flag = 0;
 
 void fatal(char *str){
 	perror(str);
@@ -14,53 +15,60 @@ void fatal(char *str){
 }
 
 int makelist(char *s, const char *delimiters, char** list, int MAX_LIST){	
-  int i = 0;
-  int numtokens = 0;
-  char *snew = NULL;
+	int i = 0;
+	int numtokens = 0;
+	char *snew = NULL;
 
-  if( (s==NULL) || (delimiters==NULL) ) return -1;
+	if( (s==NULL) || (delimiters==NULL) ) return -1;
 
-  snew = s + strspn(s, delimiters);	/* delimiters¸¦ skip */
-  if( (list[numtokens]=strtok(snew, delimiters)) == NULL )
-    return numtokens;
-	
-  numtokens = 1;
-  
-  while(1){
-     if( (list[numtokens]=strtok(NULL, delimiters)) == NULL)
-	break;
-     if(numtokens == (MAX_LIST-1)) return -1;
-     numtokens++;
-  }
+	snew = s + strspn(s, delimiters);	/* delimiters¸¦ skip */
+	if( (list[numtokens]=strtok(snew, delimiters)) == NULL )
+		return numtokens;
 
-  return numtokens;
+	numtokens = 1;
+
+	while(1){
+		if( (list[numtokens]=strtok(NULL, delimiters)) == NULL)
+			break;
+		if(numtokens == (MAX_LIST-1)) return -1;
+		numtokens++;
+	}
+
+	/* last commad '&' ->  background flag on */
+	if(!strcmp(cmdvector[numtokens-1], "&")){
+		background_flag = 1;
+		cmdvector[numtokens-1] = '\0';
+	}
+	else background_flag = 0;
+
+	return numtokens;
 }
 
 int main(int argc, char**argv){
-  int i=0;
-  pid_t pid;
-  while (1) {
-      
-  	fputs(prompt, stdout);
-	fgets(cmdline, BUFSIZ, stdin);
-	cmdline[ strlen(cmdline) -1] ='\0';
-  
-	makelist(cmdline, " \t", cmdvector, MAX_CMD_ARG);
-	/* exit input -> shell process exit. */
-	if(!strcmp(cmdvector[0], "exit")) exit(0);
+	int i=0;
+	pid_t pid;
+	while (1) {
+		fputs(prompt, stdout);
+		fgets(cmdline, BUFSIZ, stdin);
+		cmdline[ strlen(cmdline) -1] ='\0';
 
-	switch(pid=fork()){
-	case 0:
-		if(!strcmp(cmdvector[0], "cd") ) exit(0);
-		execvp(cmdvector[0], cmdvector);
-		fatal("main()");
-	case -1:
-  		fatal("main()");
-	default:
-		wait(NULL);
-		/* cd input -> parent process chage directory. */
-		if(!strcmp(cmdvector[0], "cd") && chdir(cmdvector[1]) == -1 ) perror("main()");
+		makelist(cmdline, " \t", cmdvector, MAX_CMD_ARG);
+		/* exit input -> shell process exit. */
+		if(!strcmp(cmdvector[0], "exit")) exit(0);
+
+		switch(pid=fork()){
+			case 0:
+				/* cd input -> no activate child process */
+				if(!strcmp(cmdvector[0], "cd") ) exit(0);
+				execvp(cmdvector[0], cmdvector);
+				fatal("main()");
+			case -1:
+				fatal("main()");
+			default:
+				if(background_flag == 0) wait(NULL);
+				/* cd input -> parent process chage directory. */
+				if(!strcmp(cmdvector[0], "cd") && chdir(cmdvector[1]) == -1 ) perror("main()");
+		}
 	}
-  }
-  return 0;
+	return 0;
 }
