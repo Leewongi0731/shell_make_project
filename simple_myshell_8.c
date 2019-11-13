3993d4c5b3c8f03abc8e4cc8ab99b20c8e2be48c
Leewongi0731 <wglee0731@naver.com>
2019-11-13 15:45:40 +0900

    add signal management, zombie management as signal

--- simple_myshell_8.c
+++ simple_myshell_8.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/wait.h>	/* zombie process management */
+#include <signal.h>     /* signal management */
 #define MAX_CMD_ARG 10
 
 const char *prompt = "myshell> ";
@@ -45,16 +46,41 @@ int makelist(char *s, const char *delimiters, char** list, int MAX_LIST){
 	return numtokens;
 }
 
+void reorderSignal(int unused){	
+	printf("\n");
+	fputs(prompt, stdout);
+	fgets(cmdline, BUFSIZ, stdin);
+	return;
+}
+
+void ignoreSignal(int signalNumber){
+	printf("\n");
+	return;
+}
+
 int main(int argc, char**argv){
 	int i=0;
 	pid_t pid;
+	
+	/* signal management */
+	struct sigaction reorder, ignore;
+	sigemptyset(&reorder.sa_mask); reorder.sa_handler = reorderSignal; reorder.sa_flags = SA_NOMASK;	
+	sigemptyset(&ignore.sa_mask); ignore.sa_handler = ignoreSignal; ignore.sa_flags = SA_NOMASK;
+	
+	/* zombie process management */
+	struct sigaction zombie_manager;
+	sigemptyset(&zombie_manager.sa_mask);
+	zombie_manager.sa_handler = SIG_IGN;
+	zombie_manager.sa_flags = SA_NOCLDSTOP;
+	sigaction(SIGCHLD, &zombie_manager , NULL);
+
 	while (1) {
+		/* shell process signal -> retry input command */
+		sigaction(SIGINT, &reorder, NULL); sigaction(SIGQUIT, &reorder, NULL); sigaction(SIGTSTP, &reorder, NULL);
+		
 		fputs(prompt, stdout);
 		fgets(cmdline, BUFSIZ, stdin);
 		cmdline[ strlen(cmdline) -1] ='\0';
-
-		/* zombie process management */
-		waitpid(-1, NULL, WNOHANG);
 
 		makelist(cmdline, " \t", cmdvector, MAX_CMD_ARG);
 		/* exit input -> shell process exit. */
@@ -62,6 +88,8 @@ int main(int argc, char**argv){
 
 		switch(pid=fork()){
 			case 0:
+				/* child process signal -> collect work */
+				signal(SIGINT, SIG_DFL); signal(SIGQUIT, SIG_DFL); signal(SIGTSTP, SIG_DFL);
 				/* cd input -> no activate child process */
 				if(!strcmp(cmdvector[0], "cd") ) exit(0);
 				execvp(cmdvector[0], cmdvector);
@@ -69,6 +97,8 @@ int main(int argc, char**argv){
 			case -1:
 				fatal("main()");
 			default:
+				/* shell process signal -> ignore */
+				sigaction(SIGINT, &ignore, NULL); sigaction(SIGQUIT, &ignore, NULL); sigaction(SIGTSTP, &ignore, NULL);
 				if(background_flag == 0) wait(NULL);
 				/* cd input -> parent process chage directory. */
 				if(!strcmp(cmdvector[0], "cd") && chdir(cmdvector[1]) == -1 ) perror("main()");
